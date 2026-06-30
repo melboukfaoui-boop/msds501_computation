12aad16c8c3838253f6bf1b01f2cd861623fdee0
Andres Cojuangco <andres@nickel.com>
2026-06-30 11:13:11 -0700

    added a try except block to find bad integer values

--- partner_b.py
+++ partner_b.py
@@ -33,7 +33,11 @@ scores = ['88', '95', 'absent', '72', 'n/a', '84']
 valid = []
 
 for i, s in enumerate(scores):
-    score = int(s)            # <-- this line crashes on bad strings
+    try:
+        score = int(s)            # <-- this line crashes on bad strings
+    except ValueError:
+        print(f'Bad at [{i}]: {s}')
+        continue
     print(f'Score [{i}]: {score}')
     valid.append(score)
 
